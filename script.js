3815a438eacfa16f4dc78054840cb3f3b0a1bd20
nooralruhaimani <nooralruhaimani97@gmail.com>
2025-08-25 03:33:41 -0700

    got updetad

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 // Create a empty array to store the names here 
+let list = [];
+
+
+
+function addName(){
+
+let inputVal = document.getElementById("nameInput").value;
+list.push(inputVal);
+
+ list.forEach(element => {
+ document.getElementById("namesList").innerHTML += "<li>" + element + "</li>";
+
+ });
+
+console.log(list);
+
+};
+
+
+
+function deleteList(){
+
+list.pop();
+
+let remover = document.getElementById("namesList");
+remover.removeChild(remover.lastChild);
+console.log(list);
+
+}
+
+
+
+
 
 
 // create a function that can add names to the array and print it in the list here 
